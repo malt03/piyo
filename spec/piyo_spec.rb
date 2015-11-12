dd1a659b05f23ad0b930dd8be1994afd255bca7e
Koji Murata <koji.murata@dena.com>
2015-11-12 23:11:21 +0900

    initial

---
+++ spec/piyo_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Piyo do
+  it 'has a version number' do
+    expect(Piyo::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
